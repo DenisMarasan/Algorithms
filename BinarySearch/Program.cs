1b6d276be00db6730a7d4f7e2de1b21fb55addf5
DenisMarasan <107041076+DenisMarasan@users.noreply.github.com>
2024-02-06 18:21:55 +0300

    Initial commit

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = new int[100];
+            var bs = new BinarySearch();
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                ints[i] = i + 1;
+            }
+
+            Console.WriteLine($"Pick an integer number from 1 to {ints.Length}");
+            int yourNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Your picked: {bs.Search(ints, yourNumber)}");
+            Console.ReadKey();
+        }
+    }
+
+    class BinarySearch
+    {
+        public int Search(int[] array, int x)
+        {
+            int tmpIndex = 0;
+            int minIndex = 0;
+            int maxIndex = array.Length - 1;
+
+            while (array[tmpIndex] != x)
+            {
+                tmpIndex = minIndex + (maxIndex - minIndex) / 2;
+
+                if (array[tmpIndex] == x)
+                    return array[tmpIndex];
+
+                Check();
+            }
+
+            void Check()
+            {
+                Console.WriteLine($"Is the number you picked greater than {array[tmpIndex]}? Y/N");
+
+                if (Console.ReadLine() == "y")
+                    minIndex = tmpIndex;
+                else
+                    maxIndex = tmpIndex;
+            }
+
+            throw new Exception($"Index {tmpIndex} is out of range");
+        }
+    }
+}
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array =
+            {
+                345,
+                6547,
+                235,
+                456,
+                567,
+                3245,
+                5436,
+                234,
+                456,
+                465
+            };
+
+            Print(array, "Unsorted array:");
+            SelectionSort(array);
+            Print(array, "Sorted array:");
+
+            Console.ReadKey();
+
+            void SelectionSort(int[] ints)
+            {
+                int minIndex = 0;
+                int tmpValue = 0;
+
+                for (int i = 0; i < ints.Length; i++)
+                {
+                    int minValue = int.MaxValue;
+                    int jStartIndex = i;
+
+                    for (int j = jStartIndex; j < ints.Length; j++)
+                    {
+                        if (ints[j] < minValue)
+                        {
+                            minValue = ints[j];
+                            minIndex = j;
+                        }
+                    }
+
+                    tmpValue = ints[jStartIndex];
+                    ints[jStartIndex] = ints[minIndex];
+                    ints[minIndex] = tmpValue;
+                }
+            }
+
+            void Print(int[] ints, string title)
+            {
+                Console.WriteLine($"{title}");
+
+                for (int i = 0; i < ints.Length; i++)
+                {
+                    Console.WriteLine(ints[i]);
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
